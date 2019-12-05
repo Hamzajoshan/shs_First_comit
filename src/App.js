3041db29696f251db651a72567c95a786b9a9118
hamzajoshan311 <yenkilarka@gmail.com>
2019-12-05 15:06:14 +0500

    having problem in sp signup

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Home from './Secens/Home'
+import SPsignup from './Secens/Blog'
+
+import Header from './Component/Header'
+// import Profile from './Component/bid'
+// import Bidding from './Component/bidding'
+// import MakeBid from './Component/makeBid'
+
+
+function App() {
+  return (
+    <div className="App"  >
+     <Home />
+     {/* <SPsignup/> */}
+     {/* <NewProject/> */}
+     {/* <CustomerSignup/> */}
+     {/* <Index/> */}
+     {/* <Contract/> */}
+
+    </div>
+  );
+}
+
+export default App;
